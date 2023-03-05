643833a0bce687387d8b8371dc7a650563fb70d0
ETOUMI <aetoumi@gmail.com>
2023-03-05 18:11:13 +0000

    Delect exercice 2 controller

--- src/main/java/com/websocket/exercicesxpninja/Exercice2.java
+++
@@ -1,9 +0,0 @@
-package com.websocket.exercicesxpninja;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/hello")
-public class Exercice2 {
-}
