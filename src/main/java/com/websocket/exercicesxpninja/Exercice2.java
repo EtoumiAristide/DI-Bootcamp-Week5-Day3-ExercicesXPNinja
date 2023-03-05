1395a2da1a180c4fb1afd3570d39bfdd33139835
ETOUMI <aetoumi@gmail.com>
2023-03-05 18:00:48 +0000

    First commit

---
+++ src/main/java/com/websocket/exercicesxpninja/Exercice2.java
@@ -0,0 +1,9 @@
+package com.websocket.exercicesxpninja;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Exercice2 {
+}
